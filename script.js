fa44a3477f65d50c048fe0611e364a816de87b96
dusandjergovic <149064873+dusandjergovic@users.noreply.github.com>
2024-04-04 10:34:19 +0200

    adding functionality to clicked rates

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
 
+    let userSelection = [];
 
     $("li").click(function() {
         $(this).css({
             'background-color': 'hsl(217, 12%, 63%)'
         })
-       let userSelection = $(this).attr('data-number');
-       
-       $("#btn").click(function () {
-            $("div.star, div.heading, div.text, div.list, #btn").hide();
+
+       userSelection.push($(this).attr('data-number'))
+
+       if(userSelection.length < 2) {
+        $("#btn").click(function () {
+            $("div.star, div.heading, div.text, div.list, .attribution, #btn").hide();
             $("#thankYou").show();
-            let para = $('<p>');
-            para.text(`You selected ${userSelection} out of 5`);
-            $("#container").append(para)
+            $("#ratedVote").show();
+            let rate = $(".rate")
+            rate.text(`You selected ${userSelection.slice(0, 1)} out of 5`);
+            $("#container").css({
+                'display': 'grid',
+                'place-items': 'center',
+                'gap': '1rem'
+            })
         })
+       }
+       
+       
     })
 
 })
